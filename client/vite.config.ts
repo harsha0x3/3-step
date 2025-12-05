21b4f86c8756ff0bd5f9aca288393e8a7c9722a2
Harsha <harshavardhancg@titan.co.in>
2025-12-05 09:57:59 +0530

    fixing vite build issue due to ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    checker({
+      typescript: false,
+    }),
+  ],
+
   server: {
     port: 8070,
     host: "0.0.0.0",

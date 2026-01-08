0d9a2c049f7a91433c73fb82d392bb61844f96fe
harsha vardhancg - (harsha cg) <harshavardhancg@titan.co.in>
2026-01-08 16:46:31 +0530

    gift card dump

--- server/dump_gift_cards.py
+++ server/dump_gift_cards.py
@@ -3,7 +3,7 @@ from sqlalchemy import select, and_
 from db.connection import get_db_conn
 from models import Candidate
 
-csv_path = r"C:\Users\harshavardhancg\Downloads\Employee MAster List_Titan with Card no(FINAL 8YRS ABOVE).csv"
+csv_path = r"C:\Users\harshavardhancg\Downloads\Final Sheet - 1378 Nos(FINAL 8YRS ABOVE - 1378 Nos) new(Final Sheet - 1378 Nos(FINAL 8Y).csv"
 
 
 def clean(val):
@@ -14,44 +14,61 @@ def clean(val):
 
 def dump_gift_card_codes():
     print("IN")
-    df = pd.read_csv(csv_path, dtype={"CROMA GIFT CARD": str})
+    df = pd.read_csv(csv_path, dtype={"CROMA Gift Card": str, "E.No": str})
     df = df.drop_duplicates()
+    print("DATA SHAPe ", df.shape)
 
     db = next(get_db_conn())  # ✅ correct
 
     try:
+        not_found_cands_count = 0
+        not_found_cands = []
+        added_cands = 0
+        no_gift_cards = []
+
         for _, row in df.iterrows():
             try:
                 candidate = db.scalar(
-                    select(Candidate).where(
-                        and_(
-                            Candidate.full_name == clean(row["Name"]),
-                            Candidate.division == clean(row["Division Name"]),
-                            Candidate.city == clean(row["City"]),
-                        )
-                    )
+                    select(Candidate).where(and_(Candidate.id == row["E.No"].strip()))
                 )
 
                 if not candidate:
+                    not_found_cands_count += 0
                     print(
-                        f"No candidate - {getattr(row, 'CROMA GIFT CARD')} - {row['Name']} - {row['Division Name']}"
+                        f"No candidate - {getattr(row, 'CROMA Gift Card')} - {row['E.No']} - {row['Name']} - {row['Division Name']}"
                     )
-                    continue
-                if candidate.gift_card_code:
-                    print(
-                        f"gift card exists id: {candidate.id} - {candidate.gift_card_code}"
+                    not_found_cands.append(
+                        f"No candidate - {getattr(row, 'CROMA Gift Card')} - {row['E.No']} - {row['Name']} - {row['Division Name']}"
                     )
                     continue
 
-                candidate.gift_card_code = row["CROMA GIFT CARD"]
+                if pd.isna(row["CROMA Gift Card"]):
+                    print(f"GIFT CARD IS EMPTY - {row['E.No']} - {row['Name']}")
+                    no_gift_cards.append(
+                        f"GIFT CARD IS EMPTY - {row['E.No']} - {row['Name']}"
+                    )
+                    continue
+                candidate.gift_card_code = row["CROMA Gift Card"]
+                db.commit()
+
+                added_cands += 1
 
             except Exception as e:
                 db.rollback()
-                # print(e)
+                print(e)
                 with open("new_errs.txt", "a", encoding="utf-8") as f:
                     f.write(f"{str(e)} - \n {str(row)} \n")
 
-        db.commit()
+        print("\n---------------------------------------\n")
+        print("not_found_cands: ", not_found_cands)
+        print("\n---------------------------------------\n")
+        print("not_found_cands len: ", len(not_found_cands))
+        print("\n---------------------------------------\n")
+        print("added_cands ", added_cands)
+        print("\n---------------------------------------\n")
+        print("no_gift_cards ", no_gift_cards)
+        print("\n---------------------------------------\n")
+
     finally:
         db.close()
 
---
+++ server/gift_card_res.md
@@ -0,0 +1,36 @@
+python dump_gift_cards.py
+$ python dump_gift_cards.py
+IN
+DATA SHAPe (1377, 25)
+GIFT CARD IS EMPTY - A00408 - Karthickeyan K
+GIFT CARD IS EMPTY - A02833 - Pradeep Kumar
+GIFT CARD IS EMPTY - A02887 - Rajeev Kumar
+GIFT CARD IS EMPTY - A00952 - Amit Kumar
+GIFT CARD IS EMPTY - A04566 - Syed Saqib
+GIFT CARD IS EMPTY - A00959 - Pinku Naskar
+No candidate - 1001340431168572 - 739695 - KESHAVA S J - Eyecare Division Chikkaballapura
+No candidate - 1001340431168571 - 739598 - BALAKRISHNA - Eyecare Division Chikkaballapura
+No candidate - 1001340431168576 - 739589 - NARAYANASWAMY M - Eyecare Division Chikkaballapura
+No candidate - 1001340431168550 - 739688 - MANJUNATHS - Eyecare Division Chikkaballapura
+No candidate - 1001340431168573 - 739673 - MANJUNATHA N - Eyecare Division Chikkaballapura
+No candidate - 1001340431168575 - 739686 - NARAYANASWAMY - Eyecare Division Chikkaballapura
+
+---
+
+not_found_cands: ['No candidate - 1001340431168572 - 739695 - KESHAVA S J - Eyecare Division Chikkaballapura', 'No cand
+idate - 1001340431168571 - 739598 - BALAKRISHNA - Eyecare Division Chikkaballapura', 'No candidate - 1001340431168576 - 739589 - NARAYANASWAMY M - Eyecare Division Chikkaballapura', 'No candidate - 1001340431168550 - 739688 - MANJUNATHS - Eyecare Division Chikkaballapura', 'No candidate - 1001340431168573 - 739673 - MANJUNATHA N - Eyecare Division Cnot_found_cands len: 6 not_found_cands len: 6
+
+---
+
+added_cands 1365
+
+---
+
+no_gift_cards ['GIFT CARD IS EMPTY - A00408 - Karthickeyan K ', 'GIFT CARD IS EMPTY - A02833 - Pradeep Kumar ', 'GIFT C
+ARD IS EMPTY - A02887 - Rajeev Kumar ', 'GIFT CARD IS EMPTY - A00952 - Amit Kumar ', 'GIFT CARD IS EMPTY - A04566 - Syed Saqib ', 'GIFT CARD IS EMPTY - A00959 - Pinku Naskar ']
+
+---
+
+(server)
+TILGE+harshavardhancg@TCLBLRCORLT5333 MINGW64 ~/CodeBase/hard_verify/server (main)
+$
